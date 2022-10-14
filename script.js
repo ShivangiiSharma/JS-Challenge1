951a26c3d78aa13631dfaaf69b16135c7b9b18bb
ShivangiiSharma <developershivangii@gmail.com>
2022-10-14 17:14:48 +0530

    modified code

--- script.js
+++ script.js
@@ -14,17 +14,21 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 highscore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   //when there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number";
+    displayMessage("No number..");
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Correct Number";
+    displayMessage("Correct Number..!");
 
     document.querySelector(".number").textContent = secretNumber;
 
@@ -39,12 +43,11 @@ document.querySelector(".check").addEventListener("click", function () {
     //When input is wrong
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent =
-        guess > secretNumber ? "Too high.." : "Too low..";
+      displayMessage(guess > secretNumber ? "Too high.." : "Too low..");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "You lost the game!";
+      displayMessage("You lost the game..");
       document.querySelector(".score").textContent = 0;
     }
   }
@@ -74,7 +77,7 @@ document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector(".message").textContent = "Start guessing..";
+  displayMessage("Start guessing..");
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";

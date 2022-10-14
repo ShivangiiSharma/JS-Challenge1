300379e49a80b952e37bc12c7aaab5337f23556b
ShivangiiSharma <developershivangii@gmail.com>
2022-10-14 11:02:19 +0530

    modified file

--- script.js
+++ script.js
@@ -10,8 +10,9 @@ document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
 */
 
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(".number").textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+const score = 20;
+document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -19,5 +20,11 @@ document.querySelector(".check").addEventListener("click", function () {
 
   if (!guess) {
     document.querySelector(".message").textContent = "No Number";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "Too High..";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "Too Low..";
   }
 });
